75709459875fd246c236a26e986350da218183f6
satvikkitm <161799828+furyydev@users.noreply.github.com>
2025-11-12 08:49:03 +0530

    Started a fuckn crazy homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,44 +1,22 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 // import 'package:tomb_stone/pages/login_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  final user = FirebaseAuth.instance.currentUser!;
+  // final user = FirebaseAuth.instance.currentUser!;
 
-  //logout button
-  void logOutUser() {
-    FirebaseAuth.instance.signOut();
-  }
+  // //logout button
+  // void logOutUser() {
+  //   FirebaseAuth.instance.signOut();
+  // }
 
-  //loading screen
 
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: logOutUser,
-            icon: Icon(Icons.logout, color: Colors.white),
-          ),
-        ],
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: Text(
-          ('Hello World\n${user.email!}'),
-          style: GoogleFonts.ubuntu(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
